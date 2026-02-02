dc92feb7296fcd277a8785273ca3ab246f3a6a66
OlehGr <granof.web@gmail.com>
2026-02-02 17:00:20 +0300

    change project configuration, add example.env

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,2 +1,18 @@
-DB_NAME = "testdb"
-DATABASE_URL = f"postgresql+asyncpg://postgres:postgres@localhost:5432/{DB_NAME}"
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_not_empty_env(key: str) -> str:
+    value = os.environ.get("DATABSE_URL")
+
+    if value is None:
+        raise ValueError(f"{key} not exist in enviroment")
+
+    return value
+
+
+DATABASE_URL = get_not_empty_env("DATABSE_URL")
+DB_NAME = DATABASE_URL.split("/").pop()
